7001b0715c7629466503fc5382e1c974a12ac0f5
Thomas Sahlin <thomas.sahlin@extendaretail.com>
2019-06-13 08:17:46 +0200

    Test

--- install.ps1
+++ install.ps1
@@ -1,17 +1,21 @@
-$dir = "C:\Microsoft\AndroidNDK64\android-ndk-r15c"
+$baseDir = "C:\Microsoft\AndroidNDK64"
+$ndkDir = "android-ndk-r15c"
 $url = "https://dl.google.com/android/repository/android-ndk-r15c-windows-x86_64.zip"
 
-if (Test-Path $dir)
+if (Test-Path "$baseDir\$ndkDir")
 {
+    Write-Host "Android NDK directory found: $baseDir\$ndkDir"
     Exit
 }
 
-New-Item -ItemType Directory -Path $dir
+New-Item -ItemType Directory -Path $baseDir
 
-$tmpFile = "C:\Microsoft\AndroidNDK64\ndk.zip"
+$tmpFile = "$baseDir\ndk.zip"
 
+Write-Host "Downloading Android NDK 15C from:`n$url"
 (New-Object System.Net.WebClient).DownloadFile($url, $tmpFile)
 
+Write-Host "Unzipping NDK"
 7z x $tmpFile
 
 Remove-Item $tmpFile
